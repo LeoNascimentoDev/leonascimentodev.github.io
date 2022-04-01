a7d88f4e3e0ff302926917ee6293b930a5fa7fb3
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-04-01 01:31:51 -0300

    update media query

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,5 +1,4 @@
 let element = document.querySelector("#content")
 element.addEventListener('mousemove', function () {
-    console.log('oi')
     element.scrollIntoView({behavior: "auto", block: "end", inline: "nearest"});
 });
