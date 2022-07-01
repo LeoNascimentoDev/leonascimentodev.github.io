f0f4149e47dff4b6cebc2d1d07e82977b9868d1e
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-07-01 20:13:59 -0300

    fix width for mobile

--- marveltimeline/marvel.js
+++ marveltimeline/marvel.js
@@ -18,3 +18,19 @@ $(function () {
             }, 500)
         })
 })
+
+
+
+
+
+let movieCard = Array.from(document.querySelectorAll('.movieCard'))
+let a = []
+let i=0
+movieCard.forEach(e => {
+    a.push(e.firstElementChild)
+    if (movieCard.indexOf(e) % 2 != 0) {
+        movieCard[i].removeChild(a[i])
+        movieCard[i].appendChild(a[i])
+    }
+   i++
+})
