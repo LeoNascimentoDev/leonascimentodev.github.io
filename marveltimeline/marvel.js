0f536a619836128a5da2314a1069011723065c80
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-07-01 21:50:10 -0300

    fix the mobile

--- marveltimeline/marvel.js
+++ marveltimeline/marvel.js
@@ -1,21 +1,35 @@
 // jquery para animar as informações sobre o filme
+$('.movieCard').children('a').children('img').addClass('notmobile')
+$('.movieCard').children('a').children('img').addClass('mobile')
+
 $(function () {
 
+    // $('.movieCard').children('a').children('img').addClass('mobile')
+
+    
     $('.info').hide(0)
     $('.movieCard').hover(
         function () {
             $(this).children('.info').show(600)
-            $(this).children('a').children('img').animate({
+            $(this).children('a').children('img').filter('.notmobile').animate({
                 opacity: '1',
                 width: '300px'
+            }, 500)
+            $(this).children('a').children('img').filter('.mobile').animate({
+                opacity: '1',
+                width: '40vw'
             }, 500)
         }
         ,
         function () {
             $(this).children('.info').hide(300)
-            $(this).children('a').children('img').animate({
+            $(this).children('a').children('img').filter('.notmobile').animate({
                 opacity: '0.2',
                 width: '250px'
+            }, 500)
+            $(this).children('a').children('img').filter('.mobile').animate({
+                opacity: '0.2',
+                width: '30vw'
             }, 500)
         })
 })
@@ -39,17 +53,23 @@ movieCard.forEach(e => {
 
 // código para retirar links quando for mobile
 
-$(function() {      
+
+
+$(function () {
     let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
 
     if (isMobile) {
 
+        $('.movieCard').children('a').children('img').removeClass('notmobile')
+
+
+        let links = []
         let a = Array.from(document.querySelectorAll('a'))
-        a.forEach(e=>{
+        a.forEach(e => {
             links.push(e.href)
             e.removeAttribute("href")
         })
 
         console.log('mobile');
     }
- });
+});
