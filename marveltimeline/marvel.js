501efbf0a69a57201ad7082cf13466bcd15394dc
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-07-01 21:19:30 -0300

    add mobile check

--- marveltimeline/marvel.js
+++ marveltimeline/marvel.js
@@ -1,3 +1,4 @@
+// jquery para animar as informações sobre o filme
 $(function () {
 
     $('.info').hide(0)
@@ -20,17 +21,35 @@ $(function () {
 })
 
 
-
+// script para fazer o texto aparecer ora para direita, ora para esquerda.
 
 
 let movieCard = Array.from(document.querySelectorAll('.movieCard'))
 let a = []
-let i=0
+let i = 0
 movieCard.forEach(e => {
     a.push(e.firstElementChild)
     if (movieCard.indexOf(e) % 2 != 0) {
         movieCard[i].removeChild(a[i])
         movieCard[i].appendChild(a[i])
     }
-   i++
+    i++
 })
+
+
+// código para retirar links quando for mobile
+
+$(function() {      
+    let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+    if (isMobile) {
+
+        let a = Array.from(document.querySelectorAll('a'))
+        a.forEach(e=>{
+            links.push(e.href)
+            e.removeAttribute("href")
+        })
+
+        console.log('mobile');
+    }
+ });
