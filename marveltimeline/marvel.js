7e03286d2fb8c56343ea6764902f083eea056a35
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-07-06 16:53:19 -0300

    update index

--- marveltimeline/marvel.js
+++ marveltimeline/marvel.js
@@ -106,17 +106,17 @@ $(function () {
 
 
 
-// let movieCard = Array.from(document.querySelectorAll('.movieCard'))
-// let a = []
-// let i = 0
-// movieCard.forEach(e => {
-//     a.push(e.firstElementChild)
-//     if (movieCard.indexOf(e) % 2 != 0) {
-//         movieCard[i].removeChild(a[i])
-//         movieCard[i].appendChild(a[i])
-//     }
-//     i++
-// })
+let movieCard = Array.from(document.querySelectorAll('.movieCard'))
+let a = []
+let i = 0
+movieCard.forEach(e => {
+    a.push(e.firstElementChild)
+    if (movieCard.indexOf(e) % 2 != 0) {
+        movieCard[i].removeChild(a[i])
+        movieCard[i].appendChild(a[i])
+    }
+    i++
+})
 
 
 
