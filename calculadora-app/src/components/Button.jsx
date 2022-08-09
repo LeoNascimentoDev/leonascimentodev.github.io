c7b0771614efa63ee48b31f258325724659c7f30
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-08-08 23:12:08 -0300

    removed calculadora-app and updated index

--- calculadora-app/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Button.css'
-
-const Buttons = props => <button 
-onClick={e=> props.click && props.click(props.value)}
-className={`button ${props.double ? 'double' : ''} ${props.operation ? 'operation' : ''}`}>{props.value}
-</button>
-export default Buttons
