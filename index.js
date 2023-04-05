bd957171b58c4ef72244dda4cf905ed24f6f9212
Leonardo Nascimento <leonascimento.dev@gmail.com>
2023-04-05 18:39:08 -0300

    updated nav filter
    Updated for catch the class to filter the cards

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ $(function () {
             if ($(e.target).attr('id') == "All") {
                 showCard(card)
             }
-            else if ($(e.target).attr('id') == card.id) {
+            // else if ($(e.target).attr('id') == card.id) {
+            //     card.style.display = "block" // poderia ser utilizada a função showCard
+            // }
+            else if ($(card).hasClass($(e.target).attr('id'))) {
                 card.style.display = "block" // poderia ser utilizada a função showCard
             }
             else {
